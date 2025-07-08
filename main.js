c566fc8a0ca39667cadb833620f2bd096a64eb8e
er1na <ern20501prg@gmail.com>
2025-07-09 00:05:06 +0900

    表示範囲を調整

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 200,
-        height: 200,
+        height: 400,
         transparent: true,
         frame: false,
-        alwaysOnTop: true,
+        alwaysOnTop: false,
         hasShadow: false,
         resizable: false,
         skipTaskbar: true,
         webPreferences: {
             contextIsolation: false,
             nodeIntegration: true,
+            webSecurity: false,
         },
     });
 
-    win.setIgnoreMouseEvents(true);
+    win.setIgnoreMouseEvents(false);
+    win.setMovable(true);
     win.loadFile(path.join('index.html'));
 }
 
